000bbb140aa8a3843d0f801f31d1255edaf4570e
Lohrainee <lohrainee.s@gmail.com>
2020-07-29 13:46:59 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/datagrupo/workshop/entities/OrderItem.java
+++ src/main/java/com/datagrupo/workshop/entities/OrderItem.java
@@ -33,7 +33,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
-	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
@@ -42,7 +42,7 @@ public class OrderItem implements Serializable {
 		id.setOrder(order);
 	}
 	
-	@JsonIgnore
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
