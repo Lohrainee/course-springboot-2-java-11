58187845b5d5546b9e1ed5b6209adb320d5fadf0
Lohrainee <lohrainee.s@gmail.com>
2020-07-29 17:07:42 -0300

    User Update

--- src/main/java/com/datagrupo/workshop/resources/UserResource.java
+++ src/main/java/com/datagrupo/workshop/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.datagrupo.workshop.entities.User;
 import com.datagrupo.workshop.services.UserService;
 import java.net.URI;
-
+import java.util.Map;
 
 @RestController
 @RequestMapping (value = "/users")
@@ -52,6 +53,12 @@ public class UserResource {
 	public ResponseEntity <Void> delete (@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping (value = "/{id}")
+	public ResponseEntity <User> update (@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 	
