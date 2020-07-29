256da8b2efe94cb456e72b18dd72168e107e3820
Lohrainee <lohrainee.s@gmail.com>
2020-07-29 08:27:40 -0300

    OrderStatus enum

--- src/main/java/com/datagrupo/workshop/entities/Order.java
+++ src/main/java/com/datagrupo/workshop/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.datagrupo.workshop.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
@@ -29,6 +30,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn (name = "client_id")
@@ -37,12 +40,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 	public Order () {
 		
 	}
+	
 
-
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus (orderStatus);
 		this.client = client;
 	}
 
@@ -63,6 +67,26 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 	public Instant getMoment() {
 		return moment;
 	}
+
+	
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+		
+	}
+
+
+
 
 
 	public void setMoment(Instant moment) {
