32db33883570e1e264dc9d3eaf3ab4386b5ac73d
Lohrainee <lohrainee.s@gmail.com>
2020-07-29 15:57:37 -0300

    User insert

--- src/main/java/com/datagrupo/workshop/resources/UserResource.java
+++ src/main/java/com/datagrupo/workshop/resources/UserResource.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.datagrupo.workshop.entities.User;
 import com.datagrupo.workshop.services.UserService;
+import java.net.URI;
+
 
 @RestController
 @RequestMapping (value = "/users")
@@ -33,7 +38,17 @@ public class UserResource {
 	}
 	
 	
-}
+	@PostMapping
+	public ResponseEntity <User> insert (@RequestBody User obj) {
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				   .buildAndExpand (obj.getId()).toUri();	
+		return ResponseEntity.created (uri).body(obj);
+	}
+	
+	}
+
 	 
 
 
