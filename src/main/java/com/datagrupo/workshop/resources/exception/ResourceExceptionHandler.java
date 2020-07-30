238ba578cd376b8a289b80cb5ab6dd9949239530
Lohrainee <lohrainee.s@gmail.com>
2020-07-30 07:34:33 -0300

    Exception handling - delete

--- src/main/java/com/datagrupo/workshop/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/datagrupo/workshop/resources/exception/ResourceExceptionHandler.java
@@ -9,16 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.datagrupo.workshop.services.exeptions.ResourceNotFoundException;
+import com.datagrupo.workshop.services.exeptions.DatabaseException;
+
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler (ResourceNotFoundException.class)
-		public ResponseEntity <StandardError> resourceNotFound (ResourceNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler (DatabaseException.class)
+		public ResponseEntity <StandardError> database (DatabaseException e, HttpServletRequest request) {
 		
-		String error = "Resource not found";
-		HttpStatus status = HttpStatus.NOT_FOUND;
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError (Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		
--- src/main/java/com/datagrupo/workshop/services/UserService.java
+++ src/main/java/com/datagrupo/workshop/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.datagrupo.workshop.depositories.UserRepository;
 import com.datagrupo.workshop.entities.User;
+import com.datagrupo.workshop.services.exeptions.DatabaseException;
 import com.datagrupo.workshop.services.exeptions.ResourceNotFoundException;
 
 
@@ -31,9 +34,15 @@ public class UserService {
 	}
 	
 	public void delete (Long id) {
+		try {
 		repository.deleteById(id);
+	} catch (EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException (id);
+	} catch (DataIntegrityViolationException e) {
+		throw new DatabaseException(e.getMessage());
 	}
-	
+}
+		
 	public User update (Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData (entity, obj);
